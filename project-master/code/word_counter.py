7ee801356dc16be5bbb553a555b8f3e9bc0fb233
LeoWirz <leonardo.wirz@epfl.ch>
2017-11-28 22:09:42 +0100

    word_counter class

---
+++ project-master/code/word_counter.py
@@ -0,0 +1,56 @@
+from collections import Counter
+import pandas as pd
+
+class word_counter:
+
+    counters_program_period = pd.DataFrame()
+
+    def __init__(self, df):
+        def list_flatter(values):
+            return [item for sublist in values for item in sublist]
+
+        self.counters_program_period = df.groupby([
+            df.program,
+            df.publicationDate.apply(lambda x: x.year)
+        ])[
+            #'genres', 'publicationChannelNames', 'thematicCorporations','thematicGeographicals','thematicPersons',
+            #'thematicThemes', 'visualCorporations', 'visualGeographicals', 'visualPersons',
+            'thematicGeographicals',
+            'thematicThemes',
+            'visualThemes'
+        ].agg(list_flatter)
+
+    def get_themes(self, fr, to, theme_type, program=-1, n_top=3):
+        rows_selection = ((self.counters_program_period.index.get_level_values('publicationDate') >= fr) &
+        (self.counters_program_period.index.get_level_values('publicationDate') <= to))
+        
+        if program != -1:
+            rows_selection = rows_selection & (self.counters_program_period.index.get_level_values('program') == program)
+            
+        temp_sum = self.counters_program_period.loc[rows_selection][theme_type].sum()
+        if not isinstance(temp_sum, list):
+            temp_sum = []
+        return Counter(temp_sum).most_common(n_top)
+
+    def get_topn_themes(self, fr, to='', theme_type='thematicThemes', program=-1,n_top = 3):
+        if not to:
+            to = fr
+        
+        columns_name = ['top' + str(i+1) for i in range(n_top)]
+        themes_per_year = pd.DataFrame(columns=columns_name)
+        
+        for i in range(fr, to+1):
+            temp_themes = self.get_themes(
+                i,
+                i+1,
+                theme_type,
+                program,
+                n_top
+            )
+
+            top_themes = [''] * n_top
+            for j in range(min(n_top, len(temp_themes))):
+                top_themes[j] = (temp_themes[j][0])
+
+            themes_per_year.loc[i] = top_themes
+        return themes_per_year
